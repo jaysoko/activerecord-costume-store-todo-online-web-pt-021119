c60242bd0a53df350a6cb6a69251c795c02f9e30
Jay Sokolowski <jaysoko@gmail.com>
2019-05-12 19:22:03 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,8 +5,8 @@ def change
   create_table :costume_stores do |t|
     t.string :name
     t.string :location
-    t.integer :inventory
-    t.integer :employees
+    t.integer :costume_inventory
+    t.integer :num_of_employees
     t.boolean :in_business
     t.datetime :opening_time
     t.datetime :closing_time
